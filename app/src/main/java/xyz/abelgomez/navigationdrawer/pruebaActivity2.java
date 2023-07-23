f315b2f24737422e39a7ddb88123fb44281e1033
Abel Gomez <abelgomezcondo@gmail.com>
2023-07-23 08:59:22 -0500

    subir img al back eje xd

--- app/src/main/java/xyz/abelgomez/navigationdrawer/pruebaActivity2.java
+++ app/src/main/java/xyz/abelgomez/navigationdrawer/pruebaActivity2.java
@@ -1,48 +1,50 @@
 package xyz.abelgomez.navigationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.FileUtils;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
+import android.widget.Toast;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import xyz.abelgomez.navigationdrawer.model.VolleyMultipartRequest;
+import xyz.abelgomez.navigationdrawer.model.FileUploadTask;
 
 public class pruebaActivity2 extends AppCompatActivity {
+
     private TextView textViewCotiId;
     private TextView txtInformacionReserva; // Agrega el TextView correspondiente
-
     private Button btnSubirIma;
-    private File f;
+    private String selectedFilePath = "";
     private static final int REQUEST_CODE_PICK_FILE = 1;
+    String fileName = "";
+    String uploadedFileName = "http://localhost:9999/file/filesImg/";
+
+    private static final int REQUEST_PERMISSIONS = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba2);
 
-        Log.d("PruebaActivity2", "La actividad se ha creado correctamente");
+        // Verificar y solicitar permisos
+        checkAndRequestPermissions();
 
         // Obtener el ID de cotización desde los extras
         Intent intent = getIntent();
@@ -57,6 +59,69 @@ public class pruebaActivity2 extends AppCompatActivity {
                 seleccionarArchivo();
             }
         });
+
+        Button btnGuardarReserva = findViewById(R.id.Guardarreserva);
+        btnGuardarReserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!selectedFilePath.isEmpty()) {
+                    // Crear una lista con el archivo seleccionado y enviarla al servidor
+                    List<File> files = new ArrayList<>();
+                    files.add(new File(selectedFilePath));
+                    System.out.println("======================================================"+uploadedFileName);
+
+                    // Llama al AsyncTask para realizar la operación de red en un hilo secundario
+                    new FileUploadTask().execute(files);
+                } else {
+                    Toast.makeText(pruebaActivity2.this, "Por favor, seleccione un archivo antes de guardar la reserva", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void checkAndRequestPermissions() {
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        List<String> permissionsToRequest = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission);
+            }
+        }
+
+        if (!permissionsToRequest.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toArray(new String[0]), REQUEST_PERMISSIONS);
+        } else {
+            // Si ya se tienen los permisos, realizar la acción deseada (por ejemplo, cargar el archivo)
+            cargarArchivo();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS) {
+            boolean allPermissionsGranted = true;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allPermissionsGranted = false;
+                    break;
+                }
+            }
+
+            if (allPermissionsGranted) {
+                // Permiso concedido, realizar la acción deseada (por ejemplo, cargar el archivo)
+                cargarArchivo();
+            } else {
+                // Permiso denegado, manejar el escenario en consecuencia (por ejemplo, mostrar un mensaje)
+                Toast.makeText(this, "Permisos denegados", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void cargarArchivo() {
+        // Implementa aquí la lógica para cargar el archivo seleccionado o cualquier otra acción que desees realizar.
+        // Por ejemplo, aquí puedes leer los datos del archivo o enviarlo al servidor.
     }
 
     @Override
@@ -67,32 +132,22 @@ public class pruebaActivity2 extends AppCompatActivity {
             Uri uri = data.getData();
 
             if (uri != null) {
-                try {
-                    InputStream inputStream = getContentResolver().openInputStream(uri);
-                    if (inputStream != null) {
-                        inputStream.close();
+                selectedFilePath = getRealPathFromURI(uri);
+                if (selectedFilePath != null) {
+                    fileName = getFileNameFromPath(selectedFilePath);
+                    Log.d("ArchivoSeleccionado", "Nombre del archivo seleccionado: " + fileName);
 
-                        String path = getPathFromUri(uri);
-                        if (path != null) {
-                            String fileName = getFileNameFromPath(path);
-                            Log.d("ArchivoSeleccionado", "Nombre del archivo seleccionado: " + fileName);
-
-                            txtInformacionReserva.setText("Archivo seleccionado: " + fileName);
-                            enviarArchivoAlServidor(path); // Llama a la función para enviar el archivo al servidor
-                        } else {
-                            Log.d("PruebaActivity2", "El path del archivo es nulo");
-                        }
-                    } else {
-                        Log.d("PruebaActivity2", "El archivo no se puede abrir");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    txtInformacionReserva.setText("Archivo seleccionado: " + fileName);
+                    uploadedFileName+=fileName;
+                } else {
+                    Log.d("PruebaActivity2", "El path del archivo es nulo");
                 }
             } else {
-                Log.d("PruebaActivity2", "El intent data no contiene Uri");
+                Log.d("PruebaActivity2", "La URI es nula");
             }
         }
     }
+
 
 
     public void seleccionarArchivo() {
@@ -100,71 +155,48 @@ public class pruebaActivity2 extends AppCompatActivity {
         intent.setType("*/*");
         startActivityForResult(intent, REQUEST_CODE_PICK_FILE);
     }
-    private String getPathFromUri(Uri uri) {
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        if (cursor != null) {
-            try {
-                cursor.moveToFirst();
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                if (column_index >= 0) {
-                    String result = cursor.getString(column_index);
-                    if (result != null) {
-                        return result;
-                    }
-                }
-            } finally {
-                cursor.close();
-            }
-        }
 
-        // Si la columna "_data" no está presente en el cursor, intentamos obtener el path de manera alternativa
-        String path = uri.getPath();
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String path = cursor.getString(column_index);
+        cursor.close();
         return path;
     }
 
 
+    private String getPathFromUri(Uri uri) {
+        String path = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
 
+        try (Cursor cursor = getContentResolver().query(uri, projection, null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                path = cursor.getString(columnIndex);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (path == null) {
+            // Si la columna "_data" no está presente en el cursor o no se pudo obtener la ruta,
+            // intentamos obtener el path de manera alternativa
+            path = uri.getPath();
+        }
+
+        return path;
+    }
 
     private String getFileNameFromPath(String path) {
         if (path != null) {
             return new File(path).getName();
         }
         return null;
-    }
-
-    private void enviarArchivoAlServidor(String filePath) {
-        String url = "http://192.168.18.4:9999/file/upload"; // Reemplaza con la URL de tu servidor
-
-        File file = new File(filePath);
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(
-                url,
-                null,
-                null,
-                "multipart/form-data", // Utiliza el nombre correcto "multipart/form-data"
-                file,
-                new Response.Listener<NetworkResponse>() {
-                    @Override
-                    public void onResponse(NetworkResponse response) {
-                        String message = new String(response.data);
-                        Log.d("ArchivoSubido", "Respuesta del servidor: " + message);
-                        // Puedes realizar acciones adicionales con la respuesta del servidor
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("ArchivoSubido", "Error al subir el archivo: " + error.toString());
-                        // Maneja el error aquí si es necesario
-                    }
-                }
-        );
-
-        // Agrega la solicitud a la cola de Volley
-        requestQueue.add(multipartRequest);
     }
 
 }
